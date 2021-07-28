02de4edd4a30f885e689811cd0daeb731814679e
KotaTsuboi <kota-tsuboi@g.ecc.u-tokyo.ac.jp>
2021-07-28 22:26:06 +0900

    wip

--- src/main/java/jp/ac/u_tokyo/iis/space/web/optimize/CalculationServlet.java
+++ src/main/java/jp/ac/u_tokyo/iis/space/web/optimize/CalculationServlet.java
@@ -6,11 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import jp.ac.u_tokyo.iis.space.optimization.algorithm.SimplexMethod;
-import jp.ac.u_tokyo.iis.space.optimization.constraint.EquationSymbol;
+import jp.ac.u_tokyo.iis.space.optimization.boundary.NonnegativeCondition;
+import jp.ac.u_tokyo.iis.space.optimization.equation.EquationSymbol;
 import jp.ac.u_tokyo.iis.space.optimization.constraint.LinearConstraint;
-import jp.ac.u_tokyo.iis.space.optimization.constraint.LinearEquation;
+import jp.ac.u_tokyo.iis.space.optimization.equation.LinearEquation;
+import jp.ac.u_tokyo.iis.space.optimization.exception.UnboundedException;
+import jp.ac.u_tokyo.iis.space.optimization.exception.UnfeasibleException;
 import jp.ac.u_tokyo.iis.space.optimization.function.LinearFunction;
 import jp.ac.u_tokyo.iis.space.optimization.problem.LinearProgrammingProblem;
 import jp.ac.u_tokyo.iis.space.optimization.solution.AbstractContinuousSolution;
@@ -45,53 +49,87 @@ public class CalculationServlet extends HttpServlet {
             }
         }
 
-        double[] costs = new double[n];
+        try {
+            double[] costs = new double[n];
 
-        for (int j = 0; j < n; j++) {
-            costs[j] = Double.parseDouble(request.getParameter("cost" + j));
-        }
-
-        LinearEquation[] equations = new LinearEquation[m];
-
-        for (int i = 0; i < m; i++) {
-            double[] coeff = new double[n];
             for (int j = 0; j < n; j++) {
-                coeff[j] = Double.parseDouble(request.getParameter(("coeff" + i) + j));
+                costs[j] = Double.parseDouble(request.getParameter("cost" + j));
             }
-            EquationSymbol symbol = EquationSymbol.parse(request.getParameter("symbol" + i));
-            double constant = Double.parseDouble(request.getParameter("const" + i));
-            equations[i] = new LinearEquation(coeff, symbol, constant);
-        }
 
-        LinearFunction linearFunction = new LinearFunction(costs);
-        LinearEquation[] linearEquations = equations;
-        boolean[] nonnegative = new boolean[n];
+            LinearEquation[] equations = new LinearEquation[m];
 
-        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                double[] coeff = new double[n];
+                for (int j = 0; j < n; j++) {
+                    coeff[j] = Double.parseDouble(request.getParameter(("coeff" + i) + j));
+                }
+                EquationSymbol symbol = EquationSymbol.parse(request.getParameter("symbol" + i));
+                double constant = Double.parseDouble(request.getParameter("const" + i));
+                equations[i] = new LinearEquation(coeff, symbol, constant);
+            }
+
+            LinearFunction linearFunction = new LinearFunction(costs);
+            LinearEquation[] linearEquations = equations;
             String[] isNonnegatives = request.getParameterValues("isNonnegative");
-            for (String isNonnegative : isNonnegatives) {
-                if (isNonnegative.equals(String.valueOf(j))) {
-                    nonnegative[j] = true;
+            ArrayList<Integer> variableIndexList = new ArrayList<>();
+
+            if (isNonnegatives != null) {
+                for (int j = 0; j < n; j++) {
+                    for (String isNonnegative : isNonnegatives) {
+                        if (isNonnegative.equals(String.valueOf(j))) {
+                            variableIndexList.add(j);
+                        }
+                    }
                 }
             }
-        }
 
-        LinearConstraint linearConstraint = new LinearConstraint(linearEquations, nonnegative);
-        LinearProgrammingProblem standardForm = new LinearProgrammingProblem(true, linearFunction, linearConstraint);
-        SimplexMethod instance = new SimplexMethod(standardForm);
-        AbstractContinuousSolution solution = instance.run();
+            int[] variableIndexes = new int[variableIndexList.size()];
 
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
+            for (int i = 0; i < variableIndexList.size(); i++) {
+                variableIndexes[i] = variableIndexList.get(i);
+            }
+
+            NonnegativeCondition boundary = new NonnegativeCondition(variableIndexes);
+
+            LinearConstraint linearConstraint = new LinearConstraint(linearEquations);
+            LinearProgrammingProblem standardForm = new LinearProgrammingProblem(true, linearFunction, linearConstraint, boundary);
+            System.out.println((standardForm.toStandardForm().toString()));
+            SimplexMethod instance = new SimplexMethod(standardForm);
+            AbstractContinuousSolution solution;
+
+            response.setContentType("text/html;charset=UTF-8");
+
+            PrintWriter out = response.getWriter();
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet CalculationServlet</title>");
+            out.println("<link rel=\"stylesheet\" href=\"style.css\"/>");
+            out.println("<script type=\"text/javascript\" async");
+            out.println("src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML\">");
+            out.println("</script>");
+            out.println("<meta name=\"viewport\" content=\"width=device-width,user-scalable=no,maximum-scale=1\"/>");
+            out.println("<title>Web-Optimize</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println(solution.toString());
-            out.println("</body>");
-            out.println("</html>");
+            out.println("<h1>Web-Optimize</h1>");
+            try {
+                solution = instance.run();
+                out.println("<h3>最適解が見つかりました</h3>");
+                out.println("<br>");
+                out.println(solution.toString());
+            } catch (UnboundedException ex) {
+                out.println("<h3>非有界です。</h3>");
+            } catch (UnfeasibleException ex) {
+                out.println("<h3>原点が実行不能です。</h3>");
+            } finally {
+                out.println("</body>");
+                out.println("</html>");
+                out.close();
+            }
+
+        } catch (NumberFormatException ex) {
+            request.setAttribute("ex", ex);
+            request.getRequestDispatcher("input.jsp").include(request, response);
         }
     }
 
--- src/main/webapp/input.jsp
+++ src/main/webapp/input.jsp
@@ -1,57 +1,79 @@
-<%-- 
-    Document   : input
-    Created on : 2021/07/09, 2:13:43
-    Author     : Kota
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" href="style.css"/>
-        <title>JSP Page</title>
+        <script type="text/javascript" async 
+                src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML">
+        </script>
+        <meta name="viewport" content="width=device-width,user-scalable=no,maximum-scale=1"/>
+        <title>Web-Optimize</title>
     </head>
     <body>
-        <h1>線形計画問題を設定してください。</h1>
+        <h1>Web-Optimize</h1>
+        <h3>線形計画問題を設定してください</h3>
         <%
-            int n = Integer.parseInt(request.getParameter("n"));
-            int m = Integer.parseInt(request.getParameter("m"));
+            int n = 0;
+            int m = 0;
+
+            try {
+                n = Integer.parseInt(request.getParameter("n"));
+                m = Integer.parseInt(request.getParameter("m"));
+            } catch (NumberFormatException ex) {
+                request.setAttribute("ex", ex);
+                request.getRequestDispatcher("index.html").include(request, response);
+            }
         %>
         <form method="GET" action="CalculationServlet">
-            <select name="type">
-                <option value="maximize">maximize</option>
-                <option value="maximize">minimize</option>
-            </select>
-            <br>
-            <% for (int j = 0; j < n; j++) { %>
-            <% if (j != 0) { %>
-            +
-            <% } %>
-            <input type="text" name="cost<%=j%>">X<%=j + 1%>
-            <% } %>
-            <br>
-            subject to
-            <br>
-            <% for (int i = 0; i < m; i++) { %>
-            <% for (int j = 0; j < n; j++) { %>
-            <% if (j != 0) { %>
-            +
-            <% } %>
-            <input type="text" name="coeff<%=i%><%=j%>">X<%=j + 1%>
-            <% } %>
-            <select name="symbol<%=i%>">
-                <option value="=">=</option>
-                <option value="<="><=</option>
-                <option value=">=">>=</option>
-            </select>
-            <input type="text" name="const<%=i%>">
-            <br>
-            <% } %>
-            <% for (int j = 0; j < n; j++) { %>
-            <input type="checkbox" name="isNonnegative" value=<%=j%>>
-            <% } %>
-            <input type="submit" value="計算開始">
+            <p>
+                <select name="type">
+                    <option value="maximize">MAXIMIZE</option>
+                    <option value="maximize">MINIMIZE</option>
+                </select>
+            </p>
+            <p>
+                <% for (int j = 0; j < n; j++) { %>
+                <% if (j != 0) { %>
+                +
+                <% }%>
+                <input type="text" name="cost<%=j%>" class="text">\(x_{<%=j + 1%>}\)
+                <% } %>
+            </p>
+            <p>SUBJECT TO</p>
+            <p>
+                <% for (int i = 0; i < m; i++) { %>
+                <% for (int j = 0; j < n; j++) { %>
+                <% if (j != 0) { %>
+                +
+                <% }%>
+                <input type="text" name="coeff<%=i%><%=j%>" class="text">\(x_{<%=j + 1%>}\)
+                <% }%>
+                <select name="symbol<%=i%>">
+                    <option value="=">=</option>
+                    <option value="<=" selected><=</option>
+                    <option value=">=">>=</option>
+                </select>
+                <input type="text" name="const<%=i%>" class="text">
+                <br>
+                <% } %>
+            </p>
+            <p>BOUND</p>
+            <p>
+                <% for (int j = 0; j < n; j++) {%>
+                <input type="checkbox" name="isNonnegative" value=<%=j%> checked>
+                \(x_{<%=j + 1%>} \geq 0\)
+                <br>
+                <% }%>
+            </p>
+            <p>
+                <input type="submit" value="START!" class="button">
+            </p>
+            <% if (request.getAttribute("ex") != null) {%>
+            <p class="error">
+                数値を入力してください
+            </p>
+            <% }%>
         </form>
     </body>
 </html>
